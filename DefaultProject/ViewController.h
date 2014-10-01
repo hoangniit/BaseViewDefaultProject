c3581f3ff38377f3b2ae5f04ae84478d8a2ed966
Vo Hoang <VoHoang@mms.local>
2014-10-01 13:26:06 +0700

    Initial Commit

---
+++ DefaultProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DefaultProject
+//
+//  Created by Vo Hoang on 10/1/14.
+//  Copyright (c) 2014 Vo Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DefaultProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DefaultProject
+//
+//  Created by Vo Hoang on 10/1/14.
+//  Copyright (c) 2014 Vo Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
