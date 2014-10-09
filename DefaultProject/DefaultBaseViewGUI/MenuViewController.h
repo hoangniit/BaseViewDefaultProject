31fbad542ca23f7219c6d839d3e107debb74fd7f
hoangniit <VoHoang@mms.local>
2014-10-09 15:37:55 +0700

    menu

---
+++ DefaultProject/DefaultBaseViewGUI/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  DefaultProject
+//
+//  Created by Vo Hoang on 10/9/14.
+//  Copyright (c) 2014 Vo Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
