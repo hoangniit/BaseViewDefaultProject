9940d6a84d264b7374d3757a2ad760c642ebabed
hoangniit <VoHoang@mms.local>
2014-10-09 17:21:07 +0700

    menu update

--- DefaultProject/DefaultBaseViewGUI/MenuViewController.h
+++ DefaultProject/DefaultBaseViewGUI/MenuViewController.h
@@ -10,4 +10,29 @@
 
 @interface MenuViewController : UIViewController
 
+#pragma mark - top panel
+typedef enum{
+    topMenuTypeDefault = 0,
+    topMenuTypeImage = 1,
+    topMenuTypeAccount = 2,
+    topMenuTypeSearch = 3,
+    topMenuTypeLogoWithText = 4,
+    topMenuTypeLogoWith2Textline = 5,
+}TopMenuType;
+
+//@property (strong,nonatomic) UIImageView *topImage;
+//@property (strong,nonatomic) UITextField *searchText;
+//@property (strong,nonatomic) UILabel *lblTextLine1;
+//@property (strong,nonatomic) UILabel *lblTextLine2;
+
+#pragma mark - bottom panel
+typedef enum{
+    bottomMenuTypeDefault = 0,
+    bottomMenu1Button = 1,
+    bottomMenu2Button = 2,
+    bottomMenu3Button = 3,
+}BottomMenuType;
+
+@property (nonatomic) BOOL showBottomPanelText;
+
 @end
--- DefaultProject/DefaultBaseViewGUI/MenuViewController.m
+++ DefaultProject/DefaultBaseViewGUI/MenuViewController.m
@@ -14,6 +14,16 @@
 
 @implementation MenuViewController
 
+#pragma mark - define
+#define is_IOS7_or_Later ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0 ? YES : NO)
+#define is_IPhone5 (([[UIScreen mainScreen] bounds].size.height - 568) ? NO : YES)
+#define is_IPhone6 (([[UIScreen mainScreen] bounds].size.height - 667) ? NO : YES)
+#define is_IPhone6Plus (([[UIScreen mainScreen] bounds].size.height - 736) ? NO : YES)
+#define is_IPad (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define is_IPhone (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define iOS6_NavHeight 44.0
+#define iOS7_or_Later_NavHeight 64.0
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
